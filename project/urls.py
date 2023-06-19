6ea9f90b6ad6d26431156b2e259e432d25c8b1c8
sandhyatandle37 <120715736+sandhyatandle37@users.noreply.github.com>
2023-06-19 15:14:39 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path as url
 
 
 urlpatterns = [
