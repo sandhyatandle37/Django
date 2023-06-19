384d43a98a1e7de5c98cfb79d37f43590b987d8b
sandhyatandle37 <120715736+sandhyatandle37@users.noreply.github.com>
2023-06-19 15:16:46 +0530

    Upd

--- project/urls.py
+++ project/urls.py
@@ -23,6 +23,4 @@ from django.urls import re_path as url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('authentication.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
